62d815db169f171aa6bba2d365c155b70afcade4
linstick <linstick@163.com>
2019-03-13 22:43:00 +0800

    完成设置页面的布局以及基本的操作逻辑

--- app/src/main/java/com/luoruiyong/caa/settings/SettingsActivity.java
+++ app/src/main/java/com/luoruiyong/caa/settings/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.luoruiyong.caa.settings;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.luoruiyong.caa.R;
 import com.luoruiyong.caa.base.BaseActivity;
+import com.luoruiyong.caa.dialog.common.DialogHelper;
+import com.luoruiyong.caa.feedback.FeedbackActivity;
 
 /**
  * Author: luoruiyong
@@ -17,6 +22,11 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
 
     private ImageView mBackIv;
     private TextView mTitleTv;
+    private LinearLayout mFeedbackLayout;
+    private LinearLayout mClearCacheLayout;
+    private TextView mCacheSizeTv;
+    private TextView mAboutUsTv;
+    private TextView mCheckUpdateTv;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -29,10 +39,30 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
     private void initView() {
         mBackIv = findViewById(R.id.iv_back);
         mTitleTv = findViewById(R.id.tv_title);
+        mFeedbackLayout = findViewById(R.id.ll_feedback_layout);
+        mClearCacheLayout = findViewById(R.id.ll_clear_cache_layout);
+        mCacheSizeTv = findViewById(R.id.tv_cache_size);
+        mAboutUsTv = findViewById(R.id.tv_about_us);
+        mCheckUpdateTv = findViewById(R.id.tv_check_for_update);
 
         mTitleTv.setText(R.string.title_settings);
 
         mBackIv.setOnClickListener(this);
+        mFeedbackLayout.setOnClickListener(this);
+        mClearCacheLayout.setOnClickListener(this);
+        mAboutUsTv.setOnClickListener(this);
+        mCheckUpdateTv.setOnClickListener(this);
+    }
+
+    private void doClearCache() {
+        // do clear
+        mCacheSizeTv.setText("");
+        Toast.makeText(this, R.string.settings_tip_clear_success, Toast.LENGTH_SHORT).show();
+    }
+
+    private void doCheckForUpdate(){
+        // do check
+        Toast.makeText(this, R.string.settings_tip_no_new_version, Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -40,6 +70,22 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
         switch (v.getId()) {
             case R.id.iv_back:
                 finish();
+                break;
+            case R.id.ll_feedback_layout:
+                startActivity(new Intent(this, FeedbackActivity.class));
+                break;
+            case R.id.ll_clear_cache_layout:
+                doClearCache();
+                break;
+            case R.id.tv_about_us:
+                DialogHelper.showConfirmDialog(
+                        this,
+                        getString(R.string.settings_str_about_us),
+                        getString(R.string.settings_tip_about_us_info),
+                        getString(R.string.common_str_ok));
+                break;
+            case R.id.tv_check_for_update:
+                doCheckForUpdate();
                 break;
         }
     }
